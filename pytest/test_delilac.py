862ff0eb54037c9c2130f73d8375aa9319d0bc51
dexters1 <igorilic03@gmail.com>
2020-07-27 17:30:20 +0200

    Added pytest

---
+++ pytest/test_delilac.py
@@ -0,0 +1,17 @@
+import pytest
+from zajednicki_deliliac import dodela_delilaca
+from zajednicki_deliliac import zajednicki_delilac
+
+num = None
+
+def setup_module(module):
+    global num
+    print("----- setup ------")
+    num = dodela_delilaca()
+
+def test_setup():
+    assert num == zajednicki_delilac(5, 15)
+
+def test_scope():
+    print(num)
+    assert num == num
---
+++ pytest/zajednicki_deliliac.py
@@ -0,0 +1,43 @@
+""" 
+Funkcija koja odredjuje zajednicki delilac dva broja
+"""
+def zajednicki_delilac(num1, num2):
+
+    while not(num1 == 0 or num2 == 0):
+        if(num1 >= num2):
+            num1 = num1 - num2
+        elif(num2 >= num1):
+            num2 = num2 - num1
+
+    if(num1 > num2):
+        return num1
+    else:
+        return num2
+
+"""
+Funkcija koja trazi dva broja za trazenje zajednickog delioca
+"""
+def dodela_delilaca():
+
+    num1 = None
+    num2 = None
+
+    while(num1 is None):
+        try:
+            num1 = int(input())
+        except ValueError:
+            print("Please write an integer for num1")
+
+    while(num2 is None):
+        try:
+            num2 = int(input())
+        except ValueError:
+            print("Please write an integer for num2")
+
+    print(zajednicki_delilac(num1, num2))
+
+    return zajednicki_delilac(num1, num2)
+
+
+if __name__ == "__main__":
+    dodela_delilaca()
