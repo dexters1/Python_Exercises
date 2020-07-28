93f280f390718c91945aaa233da1cdce06a9bc79
dexters1 <igorilic03@gmail.com>
2020-07-28 14:55:16 +0200

    Added Completed Assignment Code Generation Example 2

---
+++ example_2_dan_6/src/code_generation/c_project/main.c
@@ -0,0 +1,63 @@
+// generated using template: main.template
+//---------------------------------------------------------------------------------
+
+/*********************************************************************************
+**
+**  Module Name: main.c
+**  Description:
+**            Main file
+** 
+**  Author: Pera Petrovic
+**  Date:   28.07.2020 14:50:59
+**********************************************************************************
+
+// generated using template: include.template
+//---------------------------------------------------------------------------------
+
+#include <math.h>   
+// generated using template: function_prototypes.template
+//---------------------------------------------------------------------------------
+
+float zbir(float a, float b);
+
+float razlika(float a, float b);
+
+float proizvod(float a, float b);
+
+void stampa(float a, float b);
+
+// generated using template: functions.template
+//---------------------------------------------------------------------------------
+
+
+float zbir(float a, float b) {
+
+}
+
+float razlika(float a, float b) {
+
+}
+
+float proizvod(float a, float b) {
+
+}
+
+void stampa(float a, float b) {
+
+}
+
+	
+// generated using template: main_block.template
+//---------------------------------------------------------------------------------
+
+
+int main(int argc, char *argv[])
+{
+
+zbir(5.9, 3.0);
+razlika(6.0, 4.4);
+proizvod(7.0, 3.1);
+stampa(3.0, 5.7);
+// To be implemented...
+
+}
---
+++ example_2_dan_6/src/functions/function_spec.py
@@ -0,0 +1,36 @@
+"""
+Specification of functions we want to generate
+""" 
+
+functions = [
+             {
+              "name": "zbir",
+              "return_type": "float",
+              "include": "math.h",
+              "call_type1": "float a",
+              "call_type2": "float b"
+              },
+             {
+              "name": "razlika",
+              "return_type": "float",
+              "include": "math.h",
+              "call_type1": "float a",
+              "call_type2": "float b"
+              },
+             {
+              "name": "proizvod",
+              "return_type": "float",
+              "include": "math.h",
+              "call_type1": "float a",
+              "call_type2": "float b"
+              },
+             {
+              "name": "stampa",
+              "return_type": "void",
+              "include": "stdio.h",
+              "call_type1": "float a",
+              "call_type2": "float b"
+              },
+             ]
+
+
